f33240909c048c151bf0b31632c3fe322b475ded
bertamatu <info.berta@gmail.com>
2020-08-17 12:23:17 +0300

    Header social icons added

--- src/NavBar/Icons.js
+++ src/NavBar/Icons.js
@@ -1,7 +1,49 @@
 import React from "react";
+import FacebookIcon from "../assets/svg/navBarIconsSocial/Facebook.svg";
+import InstagramIcon from "../assets/svg/navBarIconsSocial/Instagram.svg";
+import TelegramIcon from "../assets/svg/navBarIconsSocial/Telegram.svg";
+import YoutubeIcon from "../assets/svg/navBarIconsSocial/Youtube.svg";
+
+import styled from "styled-components";
 
 const Icons = () => {
-  return <div></div>;
+  return (
+    <IconsMenu>
+      <Parallelogram>
+        <SocialIcon src={FacebookIcon} alt="facebook" />
+      </Parallelogram>
+      <Parallelogram>
+        <SocialIcon src={InstagramIcon} alt="instagram" />
+      </Parallelogram>
+      <Parallelogram>
+        <SocialIcon src={TelegramIcon} alt="telegram" />
+      </Parallelogram>
+      <Parallelogram>
+        <SocialIcon src={YoutubeIcon} alt="youtube" />
+      </Parallelogram>
+    </IconsMenu>
+  );
 };
 
 export default Icons;
+const IconsMenu = styled.section`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Parallelogram = styled.section`
+  width: 2rem;
+  height: 2.2rem;
+  transform: skew(-30deg);
+  background: #f0f0f0 0% 0% no-repeat padding-box;
+  margin-right: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SocialIcon = styled.img`
+  transform: skew(30deg);
+  text-align: center;
+  height: 0.7rem;
+`;
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Logo from "./Logo/Logo";
+import Icons from "./Icons";
 import Menu from "./Menu";
+
 import styled from "styled-components";
 
 const NavBar = () => {
   return (
     <NavigationBar>
-      <Logo />
+      <LogoIconsContactContainer>
+        <Logo />
+        <Icons />
+      </LogoIconsContactContainer>
       <Menu />
     </NavigationBar>
   );
@@ -16,4 +21,10 @@ export default NavBar;
 
 const NavigationBar = styled.header`
   margin: 0 3rem;
+`;
+
+const LogoIconsContactContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
