bacd93c9d4e70e78cdc0f7876b962459cdcea8fa
bertamatu <info.berta@gmail.com>
2020-08-17 15:40:37 +0300

    products section button and text

--- src/containers/ProductsCars.js
+++ src/containers/ProductsCars.js
@@ -2,11 +2,33 @@ import React from "react";
 import styled from "styled-components";
 
 const ProductsCars = () => {
-  return <CarsContainer></CarsContainer>;
+  return (
+    <CarsContainer>
+      <TopOffersText>Топ пропозицій</TopOffersText>
+      <ShowAllButton>Показать все</ShowAllButton>
+    </CarsContainer>
+  );
 };
 
 export default ProductsCars;
 
 const CarsContainer = styled.section`
   min-height: 100vh;
+  background: #f5f5f5 0% 0% no-repeat padding-box;
+  text-align: center;
+`;
+
+const TopOffersText = styled.p`
+  margin: 0;
+  font-size: 1.2rem;
+  font-weight: 900;
+  text-align: left;
+  padding: 4rem;
+`;
+const ShowAllButton = styled.button`
+  background: #f55448 0% 0% no-repeat padding-box;
+  border: none;
+  padding: 0.8rem 2rem;
+  border-radius: 5px;
+  color: white;
 `;
